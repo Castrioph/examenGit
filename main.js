2ddab1b3602290baefa3e9d31d93212cf6650920
Elliot <elliot.god@gmail.com>
2017-11-17 11:48:14 +0100

    Primer commit necesario en el examen de Git.

---
+++ main.js
@@ -0,0 +1,10 @@
+var num1=50;
+var num2=prompt("Intenta adivinar el valor entre 1 y 100");
+
+if(num1>num2){
+  alert("TE QUEDASTE CORTO");
+} else if( num1<num2){
+  alert("TE PASASTE");
+} else{
+  alert("ACERTASTE");
+}
